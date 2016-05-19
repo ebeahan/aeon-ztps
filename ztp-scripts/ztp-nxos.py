ee7610dc3c93b99c5a26524787af38c503f97d61
Jermey Schulman <jeremy@apstrktr.com>
2016-05-19 16:05:51 -0400

    updated ztp-nxos.py to remove hardcoded ip-addr

--- ztp-scripts/ztp-nxos.py
+++ ztp-scripts/ztp-nxos.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
-#md5sum="d3872a8829cd2bfe10252fad4b2785d5"
+#md5sum="4073a1853651e1b430dfd24a51bc96fa"
 
-from cli import *
+from cli import cli
+import re
 
-cli('copy http://172.20.80.10:5000/api/config0/nxos run vrf management')
+got = cli('show logging last 1000')
+server = re.findall('Script Server: (.*)\n', got)[-1]
+fetch = 'copy http://%s:5000/api/config0/nxos run vrf management' % server
+
+cli(fetch)
 cli('copy run volatile:poap.conf')
 cli('copy volatile:poap.conf scheduled-config')
 cli('copy run start')
