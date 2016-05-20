f4973710dc0dd21429b9141647547dad74a2cd08
Jermey Schulman <jeremy@apstrktr.com>
2016-05-20 18:07:32 -0400

    updates

--- ztp-scripts/ztp-nxos.py
+++ ztp-scripts/ztp-nxos.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#md5sum="8d2c39d3a799b0c03a1d4d9e8b700e8c"
+#md5sum="c2192282d3425f6afc76a18927355cff"
 
 from cli import cli
 import re
@@ -8,7 +8,7 @@ got = cli("show logging last 1000")
 server = re.findall("Script Server: (.*)\n", got)[-1]
 
 cli("copy http://{}:5000/api/config0/nxos run vrf management".format(server))
-cli("config ; ip host aeon-ztp {}".format(server))
+cli("config ; ip host ztp-server {}".format(server))
 
 cli("run bash sudo sed -i \'s|\(^Defaults *requiretty\)|#\1|g\' "
     "/isan/vdc_1/virtual-instance/guestshell+/rootfs/etc/sudoers 2>&1")
