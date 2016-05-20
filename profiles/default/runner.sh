6127e1f0e64d2b81f8c828ecde4fb0d4d4efba9d
Jermey Schulman <jeremy@apstrktr.com>
2016-05-20 10:23:13 -0400

    updates

---
+++ profiles/default/runner.sh
@@ -0,0 +1,4 @@
+#!/bin/python
+
+get AOS shit from command line args
+return JSON struct of data
--- web_api/ztp_celery.py
+++ web_api/ztp_celery.py
@@ -1,4 +1,6 @@
 import json
+import subprocess
+import re
 
 from celery import Celery
 from ztp_flask import app
@@ -9,8 +11,6 @@ __all__ = ['do_this_thing']
 app.config['CELERY_BROKER_URL'] = 'amqp://'
 app.config['CELERY_RESULT_BACKEND'] = 'rpc://'
 
-print "--> {}".format(app.name)
-
 celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
 celery.conf.update(app.config)
 
@@ -20,3 +20,23 @@ def do_this_thing(os_name, ip_addr):
     dev = NxosDevice(ip_addr, user='admin', passwd='admin')
     print json.dumps(dev.facts, indent=4)
     return 1
+
+@celery.task
+def do_other_thing(os_name, ip_addr):
+
+    run_args = [
+        'nxos-gatherfacts',
+        '--target %s' % ip_addr
+    ]
+
+    # must pass command as a single string; using shell=True
+    PIPE = subprocess.PIPE
+    this = subprocess.Popen(
+        ' '.join(run_args), shell=True,
+        stdout=PIPE, stderr=PIPE)
+
+    _stdout, _stderr = this.communicate()
+    rc = this.returncode
+
+    print "rc={}".format(rc)
+    print _stdout
