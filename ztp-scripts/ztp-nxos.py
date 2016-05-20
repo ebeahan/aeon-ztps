3b4a1322d661d0ea243ee02ba9beda8d28ef6a5d
Jermey Schulman <jeremy@apstrktr.com>
2016-05-20 17:59:59 -0400

    fixed the sudo issues

--- ztp-scripts/ztp-nxos.py
+++ ztp-scripts/ztp-nxos.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
-#md5sum="4073a1853651e1b430dfd24a51bc96fa"
+#md5sum="8d2c39d3a799b0c03a1d4d9e8b700e8c"
 
 from cli import cli
 import re
 
-got = cli('show logging last 1000')
-server = re.findall('Script Server: (.*)\n', got)[-1]
-fetch = 'copy http://%s:5000/api/config0/nxos run vrf management' % server
+got = cli("show logging last 1000")
+server = re.findall("Script Server: (.*)\n", got)[-1]
 
-cli(fetch)
+cli("copy http://{}:5000/api/config0/nxos run vrf management".format(server))
+cli("config ; ip host aeon-ztp {}".format(server))
+
+cli("run bash sudo sed -i \'s|\(^Defaults *requiretty\)|#\1|g\' "
+    "/isan/vdc_1/virtual-instance/guestshell+/rootfs/etc/sudoers 2>&1")
+
 cli('copy run volatile:poap.conf')
 cli('copy volatile:poap.conf scheduled-config')
 cli('copy run start')
