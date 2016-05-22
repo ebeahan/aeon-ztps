b64461bc1438736de3841216964f0997996625e6
Jermey Schulman <jeremy@apstrktr.com>
2016-05-22 16:19:58 -0400

    removed sudo enable from script

--- ztp-scripts/ztp-nxos.py
+++ ztp-scripts/ztp-nxos.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#md5sum="c2192282d3425f6afc76a18927355cff"
+#md5sum="27e5dba9d3de6a7334892c48cdba2ff2"
 
 from cli import cli
 import re
@@ -9,9 +9,6 @@ server = re.findall("Script Server: (.*)\n", got)[-1]
 
 cli("copy http://{}:5000/api/config0/nxos run vrf management".format(server))
 cli("config ; ip host ztp-server {}".format(server))
-
-cli("run bash sudo sed -i \'s|\(^Defaults *requiretty\)|#\1|g\' "
-    "/isan/vdc_1/virtual-instance/guestshell+/rootfs/etc/sudoers 2>&1")
 
 cli('copy run volatile:poap.conf')
 cli('copy volatile:poap.conf scheduled-config')
