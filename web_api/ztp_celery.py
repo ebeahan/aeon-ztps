a56d0ccde227487b83a3badd6876d1335faa2213
Jermey Schulman <jeremy@apstrktr.com>
2016-05-31 17:58:57 -0400

    updates

--- web_api/ztp_celery.py
+++ web_api/ztp_celery.py
@@ -1,12 +1,13 @@
 import json
 import subprocess
 import re
+import os
 
 from celery import Celery
 from ztp_flask import app
 import aeon.nxos as nxos
 
-__all__ = ['do_this_thing']
+__all__ = ['ztp_bootstrapper']
 
 app.config['CELERY_BROKER_URL'] = 'amqp://'
 app.config['CELERY_RESULT_BACKEND'] = 'rpc://'
@@ -14,29 +15,39 @@ app.config['CELERY_RESULT_BACKEND'] = 'rpc://'
 celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
 celery.conf.update(app.config)
 
-@celery.task
-def do_this_thing(os_name, ip_addr):
-    print "the os is {0} and the ip_addr is {1}".format(os_name, ip_addr)
-    dev = nxos.Device(ip_addr, user='admin', passwd='admin')
-    print json.dumps(dev.facts, indent=4)
-    return 1
+# @celery.task
+# def bootstrapper(os_name, ip_addr):
+#     print "the os is {0} and the ip_addr is {1}".format(os_name, ip_addr)
+#     dev = nxos.Device(ip_addr, user='admin', passwd='admin')
+#     print json.dumps(dev.facts, indent=4)
+#     return 1
 
 @celery.task
-def do_other_thing(os_name, ip_addr):
+def ztp_bootstrapper(os_name, ip_addr):
 
-    run_args = [
-        'nxos-gatherfacts',
-        '--target %s' % ip_addr
+    cmd_args = [
+        'nxos-bootstrap',
+        '--target %s' % ip_addr,
+        '--server 172.20.80.10',
+        '--topdir /home/admin/aeon-ztp/opt',
+        '-U AEON_TUSER',
+        '-P AEON_TPASSWD',
+        '--logfile /home/admin/aztp.log'
     ]
 
+    cmd_str = ' '.join(cmd_args)
+
     # must pass command as a single string; using shell=True
-    PIPE = subprocess.PIPE
-    this = subprocess.Popen(
-        ' '.join(run_args), shell=True,
-        stdout=PIPE, stderr=PIPE)
+
+    this = subprocess.Popen(cmd_str, shell=True,
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    print "starting bootstrapper[pid={pid}] [{cmd_str}]".format(
+        pid=this.pid, cmd_str=cmd_str)
 
     _stdout, _stderr = this.communicate()
     rc = this.returncode
 
     print "rc={}".format(rc)
-    print _stdout
+    print "stdout={}".format(_stdout)
+    print "stderr={}".format(_stderr)
--- ztp-scripts/ztp-nxos.py
+++ ztp-scripts/ztp-nxos.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#md5sum="f0a9e86cd1ad585be6c554ae9e457b42"
+#md5sum="47914a8888970d4ec369f466cbc6b125"
 
 from cli import cli
 import re
@@ -13,5 +13,10 @@ cli("copy http://ztp-server:5000/api/config0/nxos run vrf management")
 cli('copy run volatile:poap.conf')
 cli('copy volatile:poap.conf scheduled-config')
 cli('copy run start')
+
+# this copy triggers the register and kicks off the background
+# ZTP bootstrap process on the server.
+
+cli("copy http://ztp-server:5000/api/register/nxos volatile: vrf management")
 
 exit(0)
