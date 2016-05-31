3b59e91620d8f2fe328b2ef58cc794aed81a317b
Jermey Schulman <jeremy@apstrktr.com>
2016-05-31 13:25:32 -0400

    updates

--- ztp-scripts/ztp-nxos.py
+++ ztp-scripts/ztp-nxos.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#md5sum="27e5dba9d3de6a7334892c48cdba2ff2"
+#md5sum="f0a9e86cd1ad585be6c554ae9e457b42"
 
 from cli import cli
 import re
@@ -7,8 +7,8 @@ import re
 got = cli("show logging last 1000")
 server = re.findall("Script Server: (.*)\n", got)[-1]
 
-cli("copy http://{}:5000/api/config0/nxos run vrf management".format(server))
 cli("config ; ip host ztp-server {}".format(server))
+cli("copy http://ztp-server:5000/api/config0/nxos run vrf management")
 
 cli('copy run volatile:poap.conf')
 cli('copy volatile:poap.conf scheduled-config')
