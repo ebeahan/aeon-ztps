d1d6ad5197b1a6d4e807c9efb1b4d05381ca5048
Jermey Schulman <jeremy@apstrktr.com>
2016-06-03 15:05:44 -0400

    streamlined the ztp-nxos.py script

--- ztp-scripts/ztp-nxos.py
+++ ztp-scripts/ztp-nxos.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python
-#md5sum="47914a8888970d4ec369f466cbc6b125"
+#md5sum="db9bc9ddaa2a06a02e3433abc6cd442b"
 
 from cli import cli
 import re
 
+# get the ZTP server IP address from the logs.  no other way right now
+
 got = cli("show logging last 1000")
 server = re.findall("Script Server: (.*)\n", got)[-1]
+server = "%s:5000" % server
 
-cli("config ; ip host ztp-server {}".format(server))
-cli("copy http://ztp-server:5000/api/config0/nxos run vrf management")
+# copy the basic config to enable remote management. all other ZTP
+# tasks will be initiated by the ZTP server
 
+cli("copy http://%s/api/config0/nxos run vrf management" % server)
 cli('copy run volatile:poap.conf')
 cli('copy volatile:poap.conf scheduled-config')
-cli('copy run start')
 
 # this copy triggers the register and kicks off the background
 # ZTP bootstrap process on the server.
 
-cli("copy http://ztp-server:5000/api/register/nxos volatile: vrf management")
+cli("copy http://%s/api/register/nxos volatile: vrf management" % server)
 
 exit(0)
