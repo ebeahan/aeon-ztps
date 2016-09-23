06547dd66fef33960d9ed0c6369c347e5ce956a4
Bobby Watson <bobby@apstra.com>
2016-09-23 14:47:34 -0400

    Increased version

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ class Tox(TestCommand):
 
 setup(
     name="aeon-ztp",
-    version="0.2.1",
+    version="0.3.1",
     author="Jeremy Schulman",
     author_email="jeremy@apstra.com",
     description=("AEON ZTP Server"),
