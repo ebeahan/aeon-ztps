67cc54a7fd425d8264d821a804d6a49475b0fa7a
Bobby Watson <bobbywatson3@users.noreply.github.com>
2017-03-07 13:56:33 -0500

    More tests (#178)
    * aztp_os_selector and ztp_os_selector tests complete

--- tests/test_aztp_os_selector.py
+++ tests/test_aztp_os_selector.py
@@ -1,14 +1,17 @@
 import os
 import yaml
 import tempfile
-import pytest
 import json
 import sys
 import copy
-from aeon_ztp.bin import aztp_os_selector
 from collections import namedtuple
+import pytest
+from mock import patch
 
-g_dev_data = {
+from aeon_ztp.bin import aztp_os_selector
+
+
+dev_data = {
     'os_name': 'cumulus-vx',
     'vendor': 'cumulus',
     'hw_part_number': '1234',
@@ -23,7 +26,7 @@ g_dev_data = {
     'hw_model': 'cvx1000'
 }
 
-g_cfg_data = {
+cfg_data = {
     'default': {
         'regex_match': '3\.1\.[12]',
         'image': 'CumulusLinux-3.1.2-amd64.bin'
@@ -39,6 +42,14 @@ g_cfg_data = {
 }
 
 
+@pytest.fixture()
+def cli_args():
+
+    parse = aztp_os_selector.cli_parse(['--json', json.dumps(dev_data),
+                                        '--config', 'os-selector.cfg'])
+    return parse
+
+
 def os_sel_file(contents=None):
     """
     Used to create a temporary os-selector file
@@ -46,7 +57,7 @@ def os_sel_file(contents=None):
     :return: returns a temporary file string
     """
     if not contents:
-        contents = g_cfg_data
+        contents = cfg_data
     os_sel = tempfile.NamedTemporaryFile(mode='w+t')
     os_sel.file.write(yaml.dump(contents, default_flow_style=False))
     os_sel.file.flush()
@@ -129,23 +140,23 @@ def test_load_cfg_bad_syntax():
 
 
 def test_match_hw_model():
-    match = aztp_os_selector.match_hw_model(g_dev_data, g_cfg_data)
+    match = aztp_os_selector.match_hw_model(dev_data, cfg_data)
     assert match[0] == 'group_a'
-    assert match[1] == g_cfg_data['group_a']
+    assert match[1] == cfg_data['group_a']
 
 
 def test_match_hw_model_no_match():
-    new_dev_data = copy.deepcopy(g_dev_data)
+    new_dev_data = copy.deepcopy(dev_data)
     new_dev_data['hw_model'] = 'cvx2000'
-    match = aztp_os_selector.match_hw_model(new_dev_data, g_cfg_data)
+    match = aztp_os_selector.match_hw_model(new_dev_data, cfg_data)
     assert match[0] == 'default'
-    assert match[1] == g_cfg_data['default']
+    assert match[1] == cfg_data['default']
 
 
 def test_match_hw_model_no_default():
-    new_dev_data = copy.deepcopy(g_dev_data)
+    new_dev_data = copy.deepcopy(dev_data)
     new_dev_data['hw_model'] = 'cvx2000'
-    new_cfg_data = copy.deepcopy(g_cfg_data)
+    new_cfg_data = copy.deepcopy(cfg_data)
     new_cfg_data.pop('default')
     try:
         aztp_os_selector.match_hw_model(new_dev_data, new_cfg_data)
@@ -155,7 +166,7 @@ def test_match_hw_model_no_default():
 
 
 def test_match_hw_model_multi_match():
-    new_cfg_data = copy.deepcopy(g_cfg_data)
+    new_cfg_data = copy.deepcopy(cfg_data)
     new_cfg_data['group_b'] = {
         'regex_match': '3\.1\.[12]',
         'image': 'CumulusLinux-3.1.2-amd64.bin',
@@ -165,7 +176,7 @@ def test_match_hw_model_multi_match():
         }
     }
     try:
-        aztp_os_selector.match_hw_model(g_dev_data, new_cfg_data)
+        aztp_os_selector.match_hw_model(dev_data, new_cfg_data)
     except aztp_os_selector.HwMultiMatchError as e:
         pass
     assert isinstance(e, aztp_os_selector.HwMultiMatchError)
@@ -173,35 +184,133 @@ def test_match_hw_model_multi_match():
 
 def test_match_os_version_regex_no_upgrade():
     item_match = namedtuple('item_match', ['hw_match', 'data'])
-    hw_match = item_match('group_a', g_cfg_data['group_a'])
-    upgrade = aztp_os_selector.match_os_version(g_dev_data, hw_match.data)
+    hw_match = item_match('group_a', cfg_data['group_a'])
+    upgrade = aztp_os_selector.match_os_version(dev_data, hw_match.data)
     assert not upgrade
 
 
 def test_match_os_version_exact_match_no_upgrade():
-    new_cfg_data = copy.deepcopy(g_cfg_data)
+    new_cfg_data = copy.deepcopy(cfg_data)
     new_cfg_data['group_a'].pop('regex_match')
     new_cfg_data['group_a']['exact_match'] = '3.1.1'
     item_match = namedtuple('item_match', ['hw_match', 'data'])
     hw_match = item_match('group_a', new_cfg_data['group_a'])
-    upgrade = aztp_os_selector.match_os_version(g_dev_data, hw_match.data)
+    upgrade = aztp_os_selector.match_os_version(dev_data, hw_match.data)
     assert not upgrade
 
 
 def test_match_os_version_regex_upgrade():
-    new_cfg_data = copy.deepcopy(g_cfg_data)
+    new_cfg_data = copy.deepcopy(cfg_data)
     new_cfg_data['group_a']['regex_match'] = '3\.1\.[23]'
     item_match = namedtuple('item_match', ['hw_match', 'data'])
     hw_match = item_match('group_a', new_cfg_data['group_a'])
-    upgrade = aztp_os_selector.match_os_version(g_dev_data, hw_match.data)
+    upgrade = aztp_os_selector.match_os_version(dev_data, hw_match.data)
     assert upgrade == new_cfg_data['group_a']['image']
 
 
 def test_match_os_version_exact_match_upgrade():
-    new_cfg_data = copy.deepcopy(g_cfg_data)
+    new_cfg_data = copy.deepcopy(cfg_data)
     new_cfg_data['group_a'].pop('regex_match')
     new_cfg_data['group_a']['exact_match'] = '3.1.0'
     item_match = namedtuple('item_match', ['hw_match', 'data'])
     hw_match = item_match('group_a', new_cfg_data['group_a'])
-    upgrade = aztp_os_selector.match_os_version(g_dev_data, hw_match.data)
+    upgrade = aztp_os_selector.match_os_version(dev_data, hw_match.data)
     assert upgrade == new_cfg_data['group_a']['image']
+
+
+def test_match_os_version_cfgerror():
+    with pytest.raises(aztp_os_selector.CfgError):
+        dev_data = {'os_version': '1.A'}
+        hw_match = []
+        aztp_os_selector.match_os_version(dev_data, hw_match)
+
+
+@patch('aeon_ztp.bin.aztp_os_selector.exit_results', side_effect=SystemExit)
+@patch('aeon_ztp.bin.aztp_os_selector.json.loads')
+@patch('aeon_ztp.bin.aztp_os_selector.load_cfg', return_value=cfg_data)
+@patch('aeon_ztp.bin.aztp_os_selector.cli_parse')
+def test_main_json_error(mock_cli_parse, mock_load_cfg, mock_json_load, mock_exit_results, cli_args):
+    mock_json_load.side_effect = ValueError()
+    mock_cli_parse.return_value = cli_args
+    with pytest.raises(SystemExit):
+        aztp_os_selector.main()
+    mock_exit_results.assert_called_with({'ok': False,
+                                          'error_type': 'args',
+                                          'error_message': 'JSON argument formatted incorrectly.'})
+
+
+@patch('aeon_ztp.bin.aztp_os_selector.exit_results', side_effect=SystemExit)
+@patch('aeon_ztp.bin.aztp_os_selector.cli_parse')
+def test_main_argparse_error(mock_cli_parse, mock_exit_results, cli_args):
+    errmsg = 'test parse error'
+    mock_cli_parse.side_effect = aztp_os_selector.ArgumentParser.ParserError(errmsg)
+    with pytest.raises(SystemExit):
+        aztp_os_selector.main()
+    mock_exit_results.assert_called_with({'ok': False,
+                                          'error_type': 'args',
+                                          'error_message': errmsg})
+
+
+@patch('aeon_ztp.bin.aztp_os_selector.match_hw_model', side_effect=aztp_os_selector.HwNoMatchError)
+@patch('aeon_ztp.bin.aztp_os_selector.exit_results', side_effect=SystemExit)
+@patch('aeon_ztp.bin.aztp_os_selector.json.loads')
+@patch('aeon_ztp.bin.aztp_os_selector.load_cfg', return_value=cfg_data)
+@patch('aeon_ztp.bin.aztp_os_selector.cli_parse')
+def test_main_hwnomatch_error(mock_cli_parse, mock_load_cfg, mock_json_load, mock_exit_results, mock_hw_match, cli_args):
+    errmsg = 'no matching hw_model value'
+    mock_cli_parse.return_value = cli_args
+    with pytest.raises(SystemExit):
+        aztp_os_selector.main()
+    mock_exit_results.assert_called_with({'ok': False,
+                                          'error_type': 'hw_match',
+                                          'error_message': errmsg})
+
+
+@patch('aeon_ztp.bin.aztp_os_selector.match_hw_model', side_effect=aztp_os_selector.HwMultiMatchError)
+@patch('aeon_ztp.bin.aztp_os_selector.exit_results', side_effect=SystemExit)
+@patch('aeon_ztp.bin.aztp_os_selector.json.loads')
+@patch('aeon_ztp.bin.aztp_os_selector.load_cfg', return_value=cfg_data)
+@patch('aeon_ztp.bin.aztp_os_selector.cli_parse')
+def test_main_hwmultimatch_error(mock_cli_parse, mock_load_cfg, mock_json_load, mock_exit_results, mock_hw_match, cli_args):
+    errmsg = 'matches multiple os-selector groups'
+    mock_hw_match.side_effect = aztp_os_selector.HwMultiMatchError(errmsg)
+    mock_cli_parse.return_value = cli_args
+    with pytest.raises(SystemExit):
+        aztp_os_selector.main()
+    mock_exit_results.assert_called_with({'ok': False,
+                                          'error_type': 'hw_match',
+                                          'error_message': errmsg})
+
+
+@patch('aeon_ztp.bin.aztp_os_selector.match_os_version')
+@patch('aeon_ztp.bin.aztp_os_selector.match_hw_model')
+@patch('aeon_ztp.bin.aztp_os_selector.exit_results', side_effect=SystemExit)
+@patch('aeon_ztp.bin.aztp_os_selector.json.loads')
+@patch('aeon_ztp.bin.aztp_os_selector.load_cfg', return_value=cfg_data)
+@patch('aeon_ztp.bin.aztp_os_selector.cli_parse')
+def test_main_cfgerror(mock_cli_parse, mock_load_cfg, mock_json_load, mock_exit_results, mock_hw_match,
+                       mock_os_match, cli_args):
+    errmsg = 'Expecting one of'
+    mock_os_match.side_effect = aztp_os_selector.CfgError(errmsg)
+    mock_cli_parse.return_value = cli_args
+    with pytest.raises(SystemExit):
+        aztp_os_selector.main()
+    mock_exit_results.assert_called_with({'ok': False,
+                                          'error_type': 'cfg_error',
+                                          'error_message': errmsg})
+
+
+@patch('aeon_ztp.bin.aztp_os_selector.match_os_version')
+@patch('aeon_ztp.bin.aztp_os_selector.match_hw_model')
+@patch('aeon_ztp.bin.aztp_os_selector.exit_results', side_effect=SystemExit)
+@patch('aeon_ztp.bin.aztp_os_selector.json.loads')
+@patch('aeon_ztp.bin.aztp_os_selector.load_cfg', return_value=cfg_data)
+@patch('aeon_ztp.bin.aztp_os_selector.cli_parse')
+def test_main(mock_cli_parse, mock_load_cfg, mock_json_load, mock_exit_results, mock_hw_match,
+              mock_os_match, cli_args):
+    sw_match = '1.0.0'
+    mock_os_match.return_value = sw_match
+    mock_cli_parse.return_value = cli_args
+    with pytest.raises(SystemExit):
+        aztp_os_selector.main()
+    mock_exit_results.assert_called_with({'ok': True, 'image': sw_match})
