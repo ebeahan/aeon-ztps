272c6cf717abb0c3787788ab05e3ecc50d2e673f
Bobby Watson <bobbywatson3@users.noreply.github.com>
2017-07-17 12:32:21 -0400

    Update version

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ class Tox(TestCommand):
 
 setup(
     name="aeon-ztp",
-    version="1.2.0",
+    version="1.3.0",
     author="Apstra Customer Enablement",
     author_email="community@apstra.com",
     description=("AEON ZTP Server"),
